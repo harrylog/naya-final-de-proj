d9b41cb44e1e8fb40eea978e038cef77d7352171
harry <harry8log@gmail.com>
2025-08-28 12:41:16 +0300

    update

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -142,3 +142,185 @@ resource "aws_glue_job" "product_etl" {
 
   depends_on = [aws_s3_object.glue_script]
 }
+
+# ADD THESE TO modules/glue/main.tf (after the existing crawler)
+
+# Customer crawler
+resource "aws_glue_crawler" "customer_crawler" {
+  database_name = aws_glue_catalog_database.glue_database.name
+  name         = "de-proj-customer-crawler"
+  role         = aws_iam_role.glue_role.arn
+
+  s3_target {
+    path = "s3://${var.s3_bucket_name}/bronze_data/dev/Customer/"
+  }
+
+  table_prefix = var.table_prefix
+  tags = var.common_tags
+}
+
+# Orders crawler
+resource "aws_glue_crawler" "orders_crawler" {
+  database_name = aws_glue_catalog_database.glue_database.name
+  name         = "de-proj-orders-crawler"
+  role         = aws_iam_role.glue_role.arn
+
+  s3_target {
+    path = "s3://${var.s3_bucket_name}/bronze_data/dev/Orders/"
+  }
+
+  table_prefix = var.table_prefix
+  tags = var.common_tags
+}
+
+# OrderDetails crawler
+resource "aws_glue_crawler" "orderdetails_crawler" {
+  database_name = aws_glue_catalog_database.glue_database.name
+  name         = "de-proj-orderdetails-crawler"
+  role         = aws_iam_role.glue_role.arn
+
+  s3_target {
+    path = "s3://${var.s3_bucket_name}/bronze_data/dev/orderDetails/"
+  }
+
+  table_prefix = var.table_prefix
+  tags = var.common_tags
+}
+
+
+# ADD THESE ETL SCRIPTS TO S3 AND CREATE JOBS
+
+# Upload Customer ETL script
+resource "aws_s3_object" "customer_glue_script" {
+  bucket = var.s3_bucket_name
+  key    = "glue-scripts/raw_customer_etl_job.py"
+  source = "raw_customer_etl_job.py"
+  etag   = filemd5("raw_customer_etl_job.py")
+  tags = var.common_tags
+}
+
+# Upload Orders ETL script  
+resource "aws_s3_object" "orders_glue_script" {
+  bucket = var.s3_bucket_name
+  key    = "glue-scripts/raw_orders_etl_job.py"
+  source = "raw_orders_etl_job.py"
+  etag   = filemd5("raw_orders_etl_job.py")
+  tags = var.common_tags
+}
+
+# Upload OrderDetails ETL script
+resource "aws_s3_object" "orderdetails_glue_script" {
+  bucket = var.s3_bucket_name
+  key    = "glue-scripts/raw_orderdetails_etl_job.py"
+  source = "raw_orderdetails_etl_job.py"
+  etag   = filemd5("raw_orderdetails_etl_job.py")
+  tags = var.common_tags
+}
+
+# Customer ETL Job
+resource "aws_glue_job" "customer_etl" {
+  name         = "de-proj-customer-etl-job"
+  role_arn     = aws_iam_role.glue_role.arn
+  glue_version = "4.0"
+
+  command {
+    script_location = "s3://${var.s3_bucket_name}/glue-scripts/raw_customer_etl_job.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--job-language"                     = "python"
+    "--job-bookmark-option"             = "job-bookmark-enable"
+    "--enable-metrics"                  = ""
+    "--enable-spark-ui"                 = "true"
+    "--spark-event-logs-path"           = "s3://${var.s3_bucket_name}/sparkHistoryLogs/"
+    "--enable-job-insights"             = "false"
+    "--enable-observability-metrics"    = "true"
+    "--enable-glue-datacatalog"         = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--TempDir"                         = "s3://${var.s3_bucket_name}/temporary/"
+  }
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+
+  max_retries = 0
+  timeout     = 2880
+  worker_type = "G.1X"
+  number_of_workers = 2
+  tags = var.common_tags
+  depends_on = [aws_s3_object.customer_glue_script]
+}
+
+# Orders ETL Job
+resource "aws_glue_job" "orders_etl" {
+  name         = "de-proj-orders-etl-job"
+  role_arn     = aws_iam_role.glue_role.arn
+  glue_version = "4.0"
+
+  command {
+    script_location = "s3://${var.s3_bucket_name}/glue-scripts/raw_orders_etl_job.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--job-language"                     = "python"
+    "--job-bookmark-option"             = "job-bookmark-enable"
+    "--enable-metrics"                  = ""
+    "--enable-spark-ui"                 = "true"
+    "--spark-event-logs-path"           = "s3://${var.s3_bucket_name}/sparkHistoryLogs/"
+    "--enable-job-insights"             = "false"
+    "--enable-observability-metrics"    = "true"
+    "--enable-glue-datacatalog"         = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--TempDir"                         = "s3://${var.s3_bucket_name}/temporary/"
+  }
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+
+  max_retries = 0
+  timeout     = 2880
+  worker_type = "G.1X"
+  number_of_workers = 2
+  tags = var.common_tags
+  depends_on = [aws_s3_object.orders_glue_script]
+}
+
+# OrderDetails ETL Job
+resource "aws_glue_job" "orderdetails_etl" {
+  name         = "de-proj-orderdetails-etl-job"
+  role_arn     = aws_iam_role.glue_role.arn
+  glue_version = "4.0"
+
+  command {
+    script_location = "s3://${var.s3_bucket_name}/glue-scripts/raw_orderdetails_etl_job.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--job-language"                     = "python"
+    "--job-bookmark-option"             = "job-bookmark-enable"
+    "--enable-metrics"                  = ""
+    "--enable-spark-ui"                 = "true"
+    "--spark-event-logs-path"           = "s3://${var.s3_bucket_name}/sparkHistoryLogs/"
+    "--enable-job-insights"             = "false"
+    "--enable-observability-metrics"    = "true"
+    "--enable-glue-datacatalog"         = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--TempDir"                         = "s3://${var.s3_bucket_name}/temporary/"
+  }
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+
+  max_retries = 0
+  timeout     = 2880
+  worker_type = "G.1X"  
+  number_of_workers = 2
+  tags = var.common_tags
+  depends_on = [aws_s3_object.orderdetails_glue_script]
+}
